963ce6f0895a2eb882316802647df3665483ea5f
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-08-12 23:29:38 +0530

    added feed api

--- FrontEnd/src/routes/user.js
+++ FrontEnd/src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { UserAuth } = require("../middleware/auth");
 const ConnectionReqModel = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userRouter = express.Router()
 
@@ -59,6 +60,51 @@ userRouter.get("/user/connections", UserAuth, async(req, res) => {
 
     } catch (error) {
         res.status(400).send("Error: "+ error)
+    }
+})
+
+userRouter.get("/user/feed", UserAuth, async(req, res) => {
+    try{
+        // pagination logic
+        const page = req.query.page || 1;
+        let limit = req.query.limit || 10;
+
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page-1)*limit;
+
+
+        const user = req.userProfile;
+        const SAFE_FIELDS = ["firstName", "lastName", "age", "gender", "about", "skills", "photoUrl"];
+
+        const hiddenProfileForUser = await ConnectionReqModel.find({
+            $or:[
+                {fromUserId: user._id},
+                {toUserId: user._id}
+            ]
+        }).select("fromUserId toUserId")
+
+        const ProhibitedUserId = new Set();
+        
+        hiddenProfileForUser.forEach((item) => {
+            ProhibitedUserId.add(item.fromUserId.toString());
+            ProhibitedUserId.add(item.toUserId.toString());
+        })
+
+        const data = await User.find ({
+            $and: [
+                {_id: {$nin : Array.from(ProhibitedUserId)} },
+                {_id: {$ne : user._id}}
+            ]
+        })
+        .select(SAFE_FIELDS)
+        .skip(skip)
+        .limit(limit)
+
+        res.status(200).json({
+            data
+        })
+    } catch(err) {
+        res.status(400).send("Error: "+ err.message)
     }
 })
 
